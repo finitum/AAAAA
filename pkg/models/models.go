5c6eb9a9228711e7ce43ff309f15feab40e4a6a9
Victor Roest <victor@xirion.net>
2020-10-29 14:15:48 +0100

    Initial type defs
    Co-Authored-By: Julius de Jeu <julius@voidcorp.nl>

---
+++ pkg/models/models.go
@@ -0,0 +1,36 @@
+package models
+
+import (
+	"github.com/go-git/go-git/v5/plumbing"
+	"time"
+)
+
+// Pkg is an archlinux package
+type Pkg struct {
+	// Name is the name of the package (unique)
+	Name string
+	// RepoURL is the git repository where the PKGBUILD can be found
+	RepoURL string
+	// RepoBranch is which branch is used for updates
+	RepoBranch string
+	// KeepLastN determines how many old versions of packages are kept
+	KeepLastN int			`json:"keep_last_n,omitempty"`
+	// LastHash is the latest SHA1 retrieved from the package repo
+	LastHash plumbing.Hash
+	// UpdateFrequency determines how often the package should be updated
+	UpdateFrequency time.Duration
+
+	// TODO: Version?
+}
+
+func DefaultPkg() Pkg {
+	return Pkg{
+		KeepLastN:       2,
+		UpdateFrequency: time.Hour,
+	}
+}
+
+type User struct {
+	Username string
+	Password string
+}
---
+++ pkg/store/store.go
@@ -0,0 +1,22 @@
+package store
+
+import "github.com/finitum/AAAAA/pkg/models"
+
+type PackageStore interface {
+	GetPackage(name string) (*models.Pkg, error)
+	AddPackage(pkg *models.Pkg) error
+	DelPackage(pkg *models.Pkg) error
+	AllPackages() ([]*models.Pkg, error)
+	AllPackageNames() ([]string, error)
+}
+
+type UserStore interface {
+	GetUser(name string) (*models.User, error)
+	AddUser(user models.User) error
+	DelUser(user models.User) error
+}
+
+type Store interface {
+	PackageStore
+	UserStore
+}
