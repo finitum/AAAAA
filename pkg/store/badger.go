66c06a7fb8b274530c9ebc1cd777fb8bf632c2f0
Julius de Jeu <julius@voidcorp.nl>
2020-10-29 15:45:29 +0100

    Add User methods, add all packages methods

--- pkg/store/badger.go
+++ pkg/store/badger.go
@@ -13,6 +13,7 @@ type Badger struct {
 }
 
 const pkgPrefix = "pkg_"
+const userPrefix = "user_"
 
 func OpenBadgerStore(path string) (*Badger, error) {
 	db, err := badger.Open(badger.DefaultOptions(path))
@@ -53,32 +54,86 @@ func (b Badger) AddPackage(pkg *models.Pkg) error {
 			return errors.Wrap(err, "gob encode")
 		}
 
-		return errors.Wrap(txn.Set([]byte(pkgPrefix + pkg.Name), value.Bytes()), "badger transaction")
+		return errors.Wrap(txn.Set([]byte(pkgPrefix+pkg.Name), value.Bytes()), "badger transaction")
 	})
 }
 
 func (b Badger) DelPackage(pkg *models.Pkg) error {
-	panic("implement me")
+	return b.db.Update(func(txn *badger.Txn) error {
+		return errors.Wrap(txn.Delete([]byte(pkgPrefix+pkg.Name)), "badger transaction")
+	})
 }
 
 func (b Badger) AllPackages() ([]*models.Pkg, error) {
-	panic("implement me")
+	pkgs := make([]*models.Pkg, 0)
+
+	return pkgs, b.db.View(func(txn *badger.Txn) error {
+		it := txn.NewIterator(badger.DefaultIteratorOptions)
+		defer it.Close()
+		prefix := []byte(pkgPrefix)
+		for it.Seek(prefix); it.ValidForPrefix(prefix); it.Next() {
+			item := it.Item()
+			var pkg *models.Pkg
+			err := item.Value(func(val []byte) error {
+				buf := bytes.NewBuffer(val)
+
+				dec := gob.NewDecoder(buf)
+				return errors.Wrap(dec.Decode(&pkg), "gob decode")
+			})
+			pkgs = append(pkgs, pkg)
+			if err != nil {
+				return errors.Wrap(err, "badger iteration")
+			}
+		}
+		return nil
+	})
 }
 
 func (b Badger) AllPackageNames() ([]string, error) {
-	panic("implement me")
+	pkgs, err := b.AllPackages()
+	if err != nil {
+		return nil, errors.Wrap(err, "all packages")
+	}
+	var pkgNames []string
+	for _, v := range pkgs {
+		pkgNames = append(pkgNames, v.Name)
+	}
+	return pkgNames, nil
 }
 
-func (b Badger) GetUser(name string) (*models.User, error) {
-	panic("implement me")
+func (b Badger) GetUser(name string) (user *models.User, err error) {
+	err = b.db.View(func(txn *badger.Txn) error {
+		item, err := txn.Get([]byte(userPrefix + name))
+		if err != nil {
+			return errors.Wrap(err, "badger get")
+		}
+
+		return errors.Wrap(item.Value(func(val []byte) error {
+			buf := bytes.NewBuffer(val)
+
+			dec := gob.NewDecoder(buf)
+			return errors.Wrap(dec.Decode(&user), "gob decode")
+		}), "badger read")
+	})
+	return
 }
 
-func (b Badger) AddUser(user models.User) error {
-	panic("implement me")
+func (b Badger) AddUser(user *models.User) error {
+	return b.db.Update(func(txn *badger.Txn) error {
+		var value bytes.Buffer
+
+		enc := gob.NewEncoder(&value)
+		err := enc.Encode(user)
+		if err != nil {
+			return errors.Wrap(err, "gob encode")
+		}
+
+		return errors.Wrap(txn.Set([]byte(userPrefix+user.Username), value.Bytes()), "badger transaction")
+	})
 }
 
-func (b Badger) DelUser(user models.User) error {
-	panic("implement me")
+func (b Badger) DelUser(user *models.User) error {
+	return b.db.Update(func(txn *badger.Txn) error {
+		return errors.Wrap(txn.Delete([]byte(userPrefix+user.Username)), "badger transaction")
+	})
 }
-
-
--- pkg/store/badger_test.go
+++ pkg/store/badger_test.go
@@ -9,7 +9,7 @@ import (
 	"time"
 )
 
-func TestBadger_AddGetPackage(t *testing.T) {
+func TestBadger_AddGetDelPackage(t *testing.T) {
 	tstPkg := models.Pkg{
 		Name:            "name",
 		RepoURL:         "url",
@@ -30,6 +30,69 @@ func TestBadger_AddGetPackage(t *testing.T) {
 	assert.NoError(t, err)
 
 	assert.Equal(t, &tstPkg, res)
+
+	assert.NoError(t, store.DelPackage(&tstPkg))
+
+	assert.NoError(t, os.RemoveAll(storePath))
+}
+
+func TestBadger_AllPackages(t *testing.T) {
+	tstPkg := models.Pkg{
+		Name:            "foo",
+		RepoURL:         "url",
+		RepoBranch:      "branch",
+		KeepLastN:       42,
+		LastHash:        plumbing.Hash{'5'},
+		UpdateFrequency: time.Hour,
+	}
+
+	tstPkg2 := models.Pkg{
+		Name:            "bar",
+		RepoURL:         "url",
+		RepoBranch:      "branch",
+		KeepLastN:       42,
+		LastHash:        plumbing.Hash{'5'},
+		UpdateFrequency: time.Hour,
+	}
+
+	storePath := os.TempDir() + "/TestBadger_AllPackages"
+	store, err := OpenBadgerStore(storePath)
+	assert.NoError(t, err)
+
+	assert.NoError(t, store.AddPackage(&tstPkg))
+	assert.NoError(t, store.AddPackage(&tstPkg2))
+
+	pkgs, err := store.AllPackages()
+	assert.NoError(t, err)
+	assert.Len(t, pkgs, 2)
+
+	pkgNames, err := store.AllPackageNames()
+	assert.NoError(t, err)
+	assert.Len(t, pkgNames, 2)
+
+	assert.NoError(t, os.RemoveAll(storePath))
+
+}
+
+func TestBadger_AddGetDelUser(t *testing.T) {
+	tstUser := models.User{
+		Username: "testkees",
+		Password: "encryptedyes?",
+	}
+
+	storePath := os.TempDir() + "/TestBadger_AddUser"
+	store, err := OpenBadgerStore(storePath)
+	assert.NoError(t, err)
+
+	err = store.AddUser(&tstUser)
+	assert.NoError(t, err)
+
+	res, err := store.GetUser(tstUser.Username)
+	assert.NoError(t, err)
+
+	assert.Equal(t, &tstUser, res)
+
+	assert.NoError(t, store.DelUser(&tstUser))
 
 	assert.NoError(t, os.RemoveAll(storePath))
 }
