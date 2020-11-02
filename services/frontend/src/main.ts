914cf21de7e05ba7446f7aeada7fe9b04804cedf
Victor Roest <victor@xirion.net>
2020-11-02 18:00:56 +0100

    vue cli create

--- services/frontend/src/main.ts
+++ services/frontend/src/main.ts
@@ -1,8 +1,7 @@
-import {createApp} from 'vue'
-import App from './App.vue'
-import './styles/tailwind.css'
+import { createApp } from "vue";
+import App from "./App.vue";
 import router from "./router";
 
 createApp(App)
-    .use(router)
-    .mount('#app')
+  .use(router)
+  .mount("#app");
