837ec81fdf332ba9ba59cc676c04c66e32c7e449
Victor Roest <victor@xirion.net>
2020-11-08 19:48:04 +0100

    Fixed tests?

--- internal/cmdutil/cmd_test.go
+++ internal/cmdutil/cmd_test.go
@@ -7,23 +7,23 @@ import (
 )
 
 func TestRunCommandStdout(t *testing.T) {
-	stdout, _, err := RunCommand("/usr/bin/echo", "hello")
+	stdout, _, err := RunCommand("/bin/bash", "-c", "builtin echo hello")
 	assert.NoError(t, err)
 	assert.Equal(t, "hello\n", stdout)
 	//assert.Empty(t, stderr)
 }
 
 func TestRunCommandStdErr(t *testing.T) {
-	_, stderr, err := RunCommand("/usr/bin/bash", "-c", "echo hello 1>&2")
+	stdout, stderr, err := RunCommand("/bin/bash", "-c", "builtin echo hello 1>&2")
 	assert.NoError(t, err)
-	//assert.Empty(t, stdout)
+	assert.Empty(t, stdout)
 	assert.Equal(t, "hello\n", stderr)
 }
 
 func TestRunCommandNolinebreak(t *testing.T) {
-	stdout, _, err := RunCommand("/usr/bin/echo", "-n", "hello")
+	stdout, stderr, err := RunCommand("/bin/bash", "-c", "builtin echo -n hello")
 	assert.NoError(t, err)
 	assert.Equal(t, "hello", stdout)
-	//assert.Empty(t, stderr)
+	assert.Empty(t, stderr)
 	fmt.Print("\n")
 }
